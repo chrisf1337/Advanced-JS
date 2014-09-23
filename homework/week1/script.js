c8432ff52ee621083cb5cdf460137146b298954f
Christopher Fu <chrisf1337@gmail.com>
2014-09-22 23:59:01 -0400

    Part 2

--- homework/week1/script.js
+++ homework/week1/script.js
@@ -1,21 +1,58 @@
 console.log("javascript working!"); // Just to test.
+
+var renderResponse = function(response) {
+  // If you don't understand line 6, you may want to read up
+  // on Javascript objects, in the slides or internet.
+  console.log(response);
+  var stories = response.data.children;
+  for(var i in stories) {
+    story = stories[i].data;
+    var elem = $("<li></li>");
+    // Your code here:
+    // In the data in the Javascript object 'story',
+    // find the title, permalink, and upvote count
+    // Then create HTML elements with jQuery (like in line 9)
+    // and append them to the #list element.
+    var link = $('<a href="http://www.reddit.com' + story.permalink + '">' + story.title + "</a>");
+    elem.text('(' + story.score + ') ');
+    elem.append(link);
+    $("#list").append(elem);
+    // Look at the JS console in Chrome to see what story looks like
+    console.log(story);
+  }
+};
+
+var renderPetitionsResponse = function(response) {
+  // If you don't understand line 6, you may want to read up
+  // on Javascript objects, in the slides or internet.
+  console.log(response);
+  var stories = response.data.children;
+  for(var i in stories) {
+    story = stories[i].data;
+    var elem = $("<li></li>");
+    // Your code here:
+    // In the data in the Javascript object 'story',
+    // find the title, permalink, and upvote count
+    // Then create HTML elements with jQuery (like in line 9)
+    // and append them to the #list element.
+    // var link = $('<a href="http://www.reddit.com' + story.permalink + '">' + story.title + "</a>");
+    // elem.text('(' + story.score + ') ');
+    // elem.append(link);
+    $("#petitions-list").append(elem);
+    // Look at the JS console in Chrome to see what story looks like
+    console.log(story);
+  }
+};
+
 $(document).ready(function() {
-  $.get("http://www.reddit.com/hot.json", function(response) {
-    // If you don't understand line 6, you may want to read up
-    // on Javascript objects, in the slides or internet.
-    console.log(response);
-    var stories = response.data.children;
-    for(var i in stories) {
-      story = stories[i].data;
-      var elem = $("<li></li>");
-      // Your code here:
-      // In the data in the Javascript object 'story',
-      // find the title, permalink, and upvote count
-      // Then create HTML elements with jQuery (like in line 9)
-      // and append them to the #list element.
-      $("#list").append(elem);
-      // Look at the JS console in Chrome to see what story looks like
-      console.log(story);
-    }
+  $.get("http://www.reddit.com/hot.json", renderResponse);
+  // $.get('https://api.whitehouse.gov/v1/petitions.json?limit=10', renderPetitionsResponse);
+
+  $("#refresh-button").click(function() {
+    console.log('button clicked');
+    $('#list').empty();
+    $.get("http://www.reddit.com/hot.json", renderResponse);
+    var time = new Date().getTime();
+    $('#list').append('Refreshed at ' + time);
   });
 });
