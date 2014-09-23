a0eb5704fb134a29097695de41fe9a41272c8a20
Christopher Fu <chrisf1337@gmail.com>
2014-09-23 16:14:58 -0400

    Server now fetches new petition.json every time it is run

--- homework/week1/simple-cors-http-server.py
+++ homework/week1/simple-cors-http-server.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python2
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 import BaseHTTPServer
+import urllib2
 
 class CORSRequestHandler (SimpleHTTPRequestHandler):
     def end_headers (self):
@@ -8,4 +9,7 @@ class CORSRequestHandler (SimpleHTTPRequestHandler):
         SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
+    page = urllib2.urlopen('https://api.whitehouse.gov/v1/petitions.json?limit=25').read()
+    with open('petitions.json', 'w') as f:
+        f.write(page)
     BaseHTTPServer.test(CORSRequestHandler, BaseHTTPServer.HTTPServer)
