0afe73136023aca5c363346fc562b01ac25ff737
Christopher Fu <chrisf1337@gmail.com>
2014-10-07 02:02:47 -0400

    HW 3 up to Part 2

--- homework/week3/todo.js
+++ homework/week3/todo.js
@@ -1,24 +1,38 @@
-angular.module("Todo", []).controller("TodoCtrl", function($scope) {
-  $scope.newItem = {}
+angular.module("Todo", []).controller("TodoCtrl", function($scope, $window) {
+  $scope.newItem = {};
   $scope.todoList = [
     { text: "Clean room" },
     { text: "Do homework"},
     { text: "Pump iron" }
-  ]
+  ];
+  $scope.undoQueue = [];
+
   $scope.addItem = function() {
     if ($scope.newItem.text) {
       $scope.todoList.push($scope.newItem);
       $scope.newItem = {};
     }
-  }
+  };
+
   $scope.toggleDone = function(index) {
     $scope.todoList[index].done = !$scope.todoList[index].done;
-  }
+    var completedItem = $scope.todoList.splice(index, 1)[0];
+    $scope.undoQueue.push(completedItem);
+  };
+
+  $scope.undo = function() {
+    if ($scope.undoQueue.length > 0)
+    {
+      var lastItem = $scope.undoQueue.pop();
+      lastItem.done = false;
+      $scope.todoList.push(lastItem);
+    }
+  };
 
   // Ex. 2 Bonus: Detect all keypresses
   angular.element($window).bind("keypress", function(e) {
     console.log(e);
     // Do something with the event here
-  })
+  });
 });
 
